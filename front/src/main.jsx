5fb3203eceae84e47b06bfd46f3ba06858dc1cad
GabrielPenise <gabbop90@hotmail.com>
2022-11-16 13:01:16 -0300

    fix Folders

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import "./assets/styles/styles.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
