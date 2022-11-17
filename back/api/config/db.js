1687a250e0f67239c35a3251a6e9f1f8abbf7b7c
alberto <albertokarri@gmail.com>
2022-11-17 16:27:48 -0300

    fix seed database

--- back/api/config/db.js
+++ back/api/config/db.js
@@ -1,14 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.SERVER_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
-);
+const db = new Sequelize("net_security", null, null, {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false,
+});
 
 module.exports = db;
