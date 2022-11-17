14723b10f733634abe8c2fa783ed75c45176c066
alberto <albertokarri@gmail.com>
2022-11-17 15:55:06 -0300

    feature seed DB

--- back/api/models/index.js
+++ back/api/models/index.js
@@ -1,10 +1,10 @@
 const Client = require("./Client");
 const Branch = require("./Branch");
-const Guards = require("./Guards")
+const Guards = require("./Guards");
 
-// Client.hasMany(Branch);
-// Branch.belongsTo(Client);
+Client.hasMany(Branch);
+Branch.belongsTo(Client);
 
-//Guards.belongsTo(Client)
+Guards.belongsTo(Client);
 
-module.exports = { Branch, Guards };
+module.exports = { Branch, Guards, Client };
