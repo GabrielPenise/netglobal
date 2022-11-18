843e47db89d7f71d77040a09ebc85948bc73fe80
carmelis <cacabeloscarmela@gmail.com>
2022-11-18 14:39:42 -0300

    se hicieron cambios en el responsive

--- front/src/components/Nadvar.jsx
+++ front/src/components/Nadvar.jsx
@@ -2,12 +2,14 @@ import Nav from "react-bootstrap/Nav";
 import logo from "../assets/styles/image/logo_azul.png";
 import style from "../assets/styles/components/Nadvar.module.scss";
 import { CiLogin } from "react-icons/ci";
-
+import {Link }from "react-router-dom";
 function Nadvar() {
   return (
     <Nav className={`${style["nav"]}  justify-content-between`}>
       <img className={style["logoNav"]} src={logo} />
+      <Link to="/home">
       <CiLogin className={style["logOut"]} />
+      </Link >
     </Nav>
   );
 }
