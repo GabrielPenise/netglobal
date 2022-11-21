fd6ab75af803b5f9137c6abb13dc84b07de0b3ff
alberto <albertokarri@gmail.com>
2022-11-21 18:18:47 -0300

    feature[back] controllers and services for branches routes

--- back/api/controllers/branches.js
+++ back/api/controllers/branches.js
@@ -19,19 +19,16 @@ class BranchesController {
       return res.status(data.status || 500).send({ message: data.message });
     }
 
+    if (!data) {
+      return res
+        .status(404)
+        .send({ message: `No existe la branch con id ${id}` });
+    }
     res.send(data);
   }
 
   static async getClientBranches(req, res) {
     const { id } = req.params;
-
-    /* const { errorGet, dataGet } = await ClientService.getSingle(id);
-
-    if (errorGet) {
-      return res
-        .status(dataGet.status || 500)
-        .send({ message: dataGet.message });
-    } */
 
     const { error, data } = await BranchesService.getClientBranches(id);
 
@@ -42,17 +39,6 @@ class BranchesController {
   }
 
   static async createBranch(req, res) {
-    const { latitude, longitude } = req.body;
-
-    const { errorCheck, dataCheck } = await BranchesService.checkLocation(
-      latitude,
-      longitude
-    );
-
-    if (errorCheck) {
-      return res.status(400).send(dataCheck);
-    }
-
     const body = req.body;
 
     const { error, data } = await BranchesService.createBranch(body);
@@ -60,21 +46,11 @@ class BranchesController {
     if (error) {
       return res.status(data.status || 500).send({ message: data.message });
     }
-
     res.status(201).send(data);
   }
 
   static async updateBranch(req, res) {
     const { id } = req.params;
-
-    const { errorGet, dataGet } = await BranchesService.getSingle(id);
-
-    if (errorGet) {
-      return res
-        .status(dataGet.status || 500)
-        .send({ message: dataGet.message });
-    }
-
     const body = req.body;
 
     const { error, data } = await BranchesService.updateBranch(id, body);
@@ -82,8 +58,18 @@ class BranchesController {
     if (error) {
       return res.status(data.status || 500).send({ message: data.message });
     }
-
     res.status(202).send(data);
+  }
+
+  static async deleteBranch(req, res) {
+    const { id } = req.params;
+
+    const { error, data } = await BranchesService.deleteBranch(id);
+
+    if (error) {
+      return res.status(data.status || 500).send({ message: data.message });
+    }
+    res.sendStatus(202);
   }
 }
 
