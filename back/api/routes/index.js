10044a1e90877c0601d6174ec05028c11b3e3240
alberto <albertokarri@gmail.com>
2022-11-22 16:52:56 -0300

    feature[back] shift routes with controllers and services

--- back/api/routes/index.js
+++ back/api/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const routerGuards = require("./guards");
 const clientRoutes = require("./client");
-const branchesRoutes = require("./branches");
+const branchRoutes = require("./branches");
+const shiftRoutes = require("./shifts");
 
 router.use("/client", clientRoutes);
 router.use("/guards", routerGuards);
-router.use("/branches", branchesRoutes);
+router.use("/branches", branchRoutes);
+router.use("/shifts", shiftRoutes);
 
 module.exports = router;
