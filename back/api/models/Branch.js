82b5c5fb96500a6a6229378002e29188f8e2084f
alberto <albertokarri@gmail.com>
2022-11-23 15:48:00 -0300

    feature[back] coordenadas a partir de una direccion con nominatim

--- back/api/models/Branch.js
+++ back/api/models/Branch.js
@@ -1,5 +1,6 @@
 const S = require("sequelize");
 const db = require("../config/db");
+const Nominatim = require("nominatim-geocoder");
 
 class Branch extends S.Model {}
 
@@ -25,15 +26,21 @@ Branch.init(
       type: S.STRING,
       allowNull: false,
     },
-    latitude: {
-      type: S.FLOAT /* La database NO toma tipo de dato GEOMETRY: PostGIS */,
+    postalcode: {
+      type: S.STRING,
       allowNull: false,
+    },
+    latitude: {
+      type: S.FLOAT,
     },
     longitude: {
       type: S.FLOAT,
-      allowNull: false,
     },
-    fullAddress: {
+    active: {
+      type: S.BOOLEAN,
+      defaultValue: true,
+    },
+    full_address: {
       type: S.VIRTUAL,
       get() {
         return `${this.street}, ${this.number}, ${this.city}, ${this.province}`;
@@ -48,5 +55,25 @@ Branch.init(
   },
   { sequelize: db, modelName: "branch" }
 );
+
+Branch.beforeCreate((branch) => {
+  const geocoder = new Nominatim();
+
+  return geocoder
+    .search({
+      street: `${branch.street} ${branch.number}`,
+      city: branch.city,
+      country: "Argentina",
+      state: branch.province,
+      postalcode: branch.postalcode,
+    })
+    .then((res) => {
+      branch.latitude = res[0].lat;
+      branch.longitude = res[0].lon;
+    })
+    .catch((error) => {
+      console.error("ERROR nominatim", error);
+    });
+});
 
 module.exports = Branch;
