a8bc3f2fb69aa7b354aa38ac023dbbf32c29f8c5
alberto <albertokarri@gmail.com>
2022-11-29 16:34:26 -0300

    adding axios to bring events to front

--- front/src/commons/Calendar/Calendario.jsx
+++ front/src/commons/Calendar/Calendario.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Axios } from "../../utils/AxiosWithCredentials";
 import moment from "moment";
 import "moment/locale/es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -13,37 +14,21 @@ import { BtnAddEvent } from "../Buttons/BtnAddEvent.jsx";
 
 const localizer = momentLocalizer(moment);
 moment.locale("es");
-// const myEventList = [
-//   {
-//     id: 1,
-//     title: "Guardia",
-//     start: moment().add(2, "day").toDate(),
-//     end: moment().add(3, "day").toDate(),
-//     branchId: 1,
-//     guardId: 1,
-//     nota: "Cubrir Puesto de trabajo",
-//     guard: {
-//       id: 1,
-//       name: "Juan Carlos",
-//       lastname: "Lopez",
-//       CUIL: 1231232,
-//       Provincia: "Buenos Aires",
-//       Localidad: "Mar del Plata",
-//     },
-//   },
-//   {
-//     title: "Chau",
-//     start: moment().add(1, "day").toDate(),
-//     end: moment().add(2, "day").toDate(),
-//     autor: "Trabajo Juan",
-//     notes: "Nota descriptiva",
-//   },
-// ];
 
 export default function Calendario() {
   const [fijarVista, setFijarVista] = useState(
     localStorage.getItem("fijarVista") || "month"
   );
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    Axios.get("/events/byClient/4")
+      .then((res) => setEvents(res.data))
+      .catch((err) => console.error(error));
+  }, []);
+  console.log(events);
+  console.log(fakeDataEvent);
 
   const handleOnview = (e) => {
     setFijarVista(e);
@@ -67,7 +52,7 @@ export default function Calendario() {
       <div style={{ height: "100vh" }}>
         <Calendar
           localizer={localizer}
-          events={fakeDataEvent}
+          events={events}
           startAccesor="start"
           endAccesor="end"
           messages={messages}
