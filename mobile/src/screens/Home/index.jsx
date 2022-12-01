1a7a6f9323f781bfceafe438d47aac9e2c92ce51
gerar10 <gerarburgos1987@gmail.com>
2022-12-01 18:48:03 -0300

    "cambios login logout"

--- mobile/src/screens/Home/index.jsx
+++ mobile/src/screens/Home/index.jsx
@@ -1,83 +1,112 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Avatar } from '@rneui/themed';
-import { Card } from '@rneui/themed';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import { Button } from '@rneui/base';
-import { ListItem } from '@rneui/themed';
-import { CardDivider } from '@rneui/base/dist/Card/Card.Divider';
-import {URLBase} from "../../url/variable";
+import { Text, View, StyleSheet } from "react-native";
+import { Avatar } from "@rneui/themed";
+import { Card } from "@rneui/themed";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Button } from "@rneui/base";
+import { URLBase } from "../../url/variable";
 
-function HomeScreen({route, navigation, perfil}) {
-const user = useSelector((state) => state.user)
-const [empleado, setEmpleado] = useState({})
-const id = user.id
+function HomeScreen({ navigation }) {
+  const user = useSelector((state) => state.user);
+  const [empleado, setEmpleado] = useState({});
+  const id = user.id;
 
+  console.log(user);
 
-useEffect(() =>{
-  if(user) 
-  URLBase.get(`/guards/${id}`).then((res) => setEmpleado(res.data))
-}, [id])
+  useEffect(() => {
+    URLBase.get(`/guards/${id}`).then((res) => setEmpleado(res.data));
+  }, []);
 
+  const removeValue = async () => {
+    try {
+      await AsyncStorage.removeItem("user");
+    } catch (e) {
+      console.log(e);
+    }
+    console.log("Done.");
+  };
 
-const removeValue = async () => {
-  try {
-    await AsyncStorage.removeItem('user')
-  } catch(e) {
-    console.log(e);
-  }
-    console.log('Done.')
-}  
+  const handleLogOut = () => {
+    removeValue();
+    alert("Sesión cerrada correctamente :)");
+    navigation.navigate("Logueo");
+  };
 
-const handleLogOut = () => {
-  removeValue()
-  navigation.navigate("Logueo")
-} 
-ListItem
   return (
-    <View style={{ flex: 1, alignItems: 'center', marginTop: 100}}>
-    <Avatar
-          size={200}
-          rounded
-          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-DIROB4u3cU82LhXPXqJIlMNeUrElZCKwKA&usqp=CAU' }}
-          title="Bj"
+    <View
+      style={{ alignContent: "center", alignItems: "center", marginTop: 100 }}
+    >
+      <Card style={{ alignItems: "center", alignItems: "center" }}>
+        <Card.Title> NET SECURITY </Card.Title>
+        <Card.Divider />
+        <View
+          style={{
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+          }}
         >
-     </Avatar>
-<CardDivider/>
-<CardDivider/>
-     <View>
-     <Text style={styles.datos}> Nombre: {empleado.name} </Text>
-     <Text style={styles.datos}> Apellido: {empleado.lastname} </Text>
-     <Text style={styles.datos}> CUIL: {empleado.cuil} </Text>
-     <Text style={styles.datos}> Domicilio: {`${empleado.street} ${empleado.number}`} </Text>
-     <Text style={styles.datos}> Ciudad: {empleado.city} </Text>
-     <Text style={styles.datos}> Provincia: {empleado.province} </Text>
-     </View>
-     <Card.Divider/>
-     <Card.Divider/>
-     <Card.Divider/>
-     <Button title='Cerrar Sesión' onPress={handleLogOut} buttonStyle={{
-  backgroundColor: 'rgba(90, 154, 230, 1)',
-  borderColor: 'transparent',
-  borderWidth: 0,
-  borderRadius: 30,
-}}/>
+          <Avatar
+            size={200}
+            rounded
+            source={{
+              uri: "https://img.freepik.com/psd-premium/avatar-masculino-profesion-policial-renderizado-3d_52659-1093.jpg?w=2000",
+            }}
+            title="Bj"
+          ></Avatar>
+        </View>
+
+        <View style={{ marginTop: 40 }}>
+          <Text style={styles.datos}> Nombre: {empleado.name} </Text>
+          <Text style={styles.datos}> Apellido: {empleado.lastname} </Text>
+          <Text style={styles.datos}> CUIL: {empleado.cuil} </Text>
+          <Text style={styles.datos}>
+            {" "}
+            Domicilio: {`${empleado.street} ${empleado.number}`}{" "}
+          </Text>
+          <Text style={styles.datos}> Ciudad: {empleado.city} </Text>
+          <Text style={styles.datos}> Provincia: {empleado.province} </Text>
+        </View>
+        <View
+          style={{
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        >
+          <Button
+            title="Cerrar Sesión"
+            onPress={handleLogOut}
+            buttonStyle={{
+              backgroundColor: "rgba(90, 154, 230, 1)",
+              borderColor: "transparent",
+              borderWidth: 0,
+              borderRadius: 30,
+              width: 150,
+              marginTop: 40,
+              alignItems: "center",
+              alignContent: "center",
+            }}
+          />
+        </View>
+      </Card>
     </View>
   );
 }
 
 export default HomeScreen;
 
-
 const styles = StyleSheet.create({
   legajo: {
-    fontWeight: '350',
-    fontSize: 30
+    fontWeight: "350",
+    fontSize: 30,
   },
   datos: {
     fontSize: 20,
-    
-  }
+    margin: 1,
+    marginLeft: 35,
+    marginRight: 35,
+  },
 });
